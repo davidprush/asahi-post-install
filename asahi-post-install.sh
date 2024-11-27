5e490e89ec005b7241b50bfde96ace0b5bd7b6ed
David P. Rush <davidprush@gmail.com>
2024-11-26 19:19:35 -0500

    Update asahi-post-install.sh
    Making it more executable. Still Broken.

--- asahi-post-install.sh
+++ asahi-post-install.sh
@@ -1,68 +1,114 @@
+#!/usr/bin/sh
+set -e
 
-# Upgrade everything
-sudo dnf update && sudo dnf upgrade
-sudo dnf install dnf-plugins-core
-sudo dnf install inxi gpg nano bpytop glances htop yt-dlp curl chromium neofetch python pip git
+if [ "$(whoami)" != "root" ]; then
+    echo "Script must be run as root" >&2
+    exit 1
+fi
 
-# Update and clean up
-sudo dnf update && sudo dnf upgrade
-sudo dnf clean all && sudo dnf autoremove
+if [ "$1" == "help" ]; then
+    echo "essential         Update system and install essential CLI apps"
+    echo "clean             Update and cleanup the system"
+    echo "swappiness        Pass a number (0-100) to increase (hi=100) or decrease (lo=0) swappiness"
+    echo "swap              Pass a number to change swap size"
+    echo "widevine          Install widevine"
+    echo "codecs            Install RPMFusion with standard plugins and codecs"
+    echo "vscode            Install Microsoft's Visual Studio Code repo and vs code app"
+    echo "brave             Install Brave Browser repo and Brave Browser app"
+    echo "konsave           Install konsave and save current kde settings/configuration"
+    exit 0
+fi
 
-# Set swap file size
-cat /proc/swaps #verify current swaps
-sudo dnf update --refresh
-sudo /usr/libexec/fedora-asahi-remix-scripts/setup-swap.sh --recreate 16G
-cat /proc/swaps #verify change
+if [ "$1" == "essential" ]; then
+    dnf update && dnf upgrade
+    dnf install dnf-plugins-core
+    dnf install inxi gpg nano bpytop glances htop yt-dlp curl chromium neofetch python pip git
+    exit 0
+fi
 
-# Adjust Swappiness 
-cat /proc/sys/vm/swappiness # get and display current swappiness
-sudo sysctl vm.swappiness=90 # Change current val (0-low swapping, 100-high swapping) resets after reboot
-sudo nano /etc/sysctl.conf # edit/append line (90 works well with MacBook Air 8GB/16GB Swap): vm.swappiness=90
+if [ "$1" == "clean" ]; then
+    dnf update && dnf upgrade
+    dnf clean all && dnf autoremove
+    exit 0
+fi
 
-# Install widevine
-sudo dnf install widevine-installer
-sudo widevine-installer
+if [ "$1" == "swappiness" ]; then
+    echo "Current swappiness:"
+    cat /proc/sys/vm/swappiness # get and display current swappiness
+    sysctl vm.swappiness=90 # Change current val (0-low swapping, 100-high swapping) resets after reboot
+    nano /etc/sysctl.conf # edit/append line (90 works well with MacBook Air 8GB/16GB Swap): vm.swappiness=90
+    echo "vm.swappiness=90" >> /etc/sysctl.conf
+    exit 0
+fi
 
-# Multimedia codec installation
-sudo dnf install   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf group update core
-sudo dnf config-manager --enable fedora-cisco-openh264
-sudo dnf update && sudo dnf upgrade
-sudo dnf clean all && sudo dnf autoremove
-sudo dnf install vlc
-sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
-sudo dnf install lame\* --exclude=lame-devel
-sudo dnf group upgrade --with-optional Multimedia
-sudo dnf install libavcodec-freeworld
-sudo dnf install dnf-plugins-core
-sudo dnf update && sudo dnf upgrade
+if [ "$1" == "swap" ]; then
+    cat /proc/swaps #verify current swaps
+    dnf update --refresh
+    sudo /usr/libexec/fedora-asahi-remix-scripts/setup-swap.sh --recreate 16G
+    cat /proc/swaps #verify change
+    exit 0
+fi
 
-# Install VS Code repos and app
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-dnf check-update
-sudo dnf install code # or code-insiders
+if [ "$1" == "widevine" ]; then
+    dnf install widevine-installer
+    sudo widevine-installer
+    exit 0
+fi
 
-# Install Brave Browser
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf install brave-browser 
+if [ "$1" == "codecs" ]; then
+    # Multimedia codec installation
+    dnf install   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+    dnf install   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    dnf group update core
+    dnf config-manager --enable fedora-cisco-openh264
+    dnf update && dnf upgrade
+    dnf clean all && dnf autoremove
+    dnf install vlc
+    dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+    dnf install lame\* --exclude=lame-devel
+    dnf group upgrade --with-optional Multimedia
+    dnf install libavcodec-freeworld
+    dnf install dnf-plugins-core
+    dnf update && dnf upgrade
+    exit 0
+fi
 
-# Optional remove Brave repos
-ls /etc/yum.repos.d/
-sudo rm /etc/yum.repos.d/brave-browser-beta.repo
-sudo rm /etc/yum.repos.d/brave-browser.repo
+if [ "$1" == "vscode" ]; then
+    # Install VS Code repos and app
+    rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+    dnf check-update
+    dnf install code # or code-insiders
+    exit 0
+fi
 
-# konsave commands for saving or restoring Plasma desktop settings
-python -m pip install konsave
-konsave -h
+if [ "$1" == "brave" ]; then
+    # Install Brave Browser
+    dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+    rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+    dnf install brave-browser
+    exit 0
+fi 
+
+if [ "$1" == "konsave" ]; then
+    # konsave commands for saving or restoring Plasma desktop settings
+    python -m pip install konsave
+    konsave -h
+fi
+
+if [ $# -eq 0 ]; then
+    echo "No command supplied. Use help to see commands."
+    exit 1
+else
+    echo "$1 is not a command. Use help to see commands."
+    exit 1
+fi
 
 # Misc Stuff
-cat /proc/sys/kernel/core_pattern
-sudo nano /etc/dnf/dnf.conf
-sudo dnf update && sudo dnf upgrade
+#cat /proc/sys/kernel/core_pattern
+#nano /etc/dnf/dnf.conf
+#dnf update && sudo dnf upgrade
 
 # USB mounts
-cd /run/media/david/
-lsblk
+#cd /run/media/david/
+#lsblk
